d987267071262d94a521c68b89266d53ace3ff0b
dev0x1 <69981185+dev0x1@users.noreply.github.com>
2021-06-28 10:44:22 +0530

    extern is not needed in rust-2018

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,1 @@
-extern crate lazy_static;
-extern crate miracl_core;
-extern crate num_bigint;
-extern crate num_integer;
-extern crate num_traits;
-extern crate rand;
-extern crate rand_chacha;
-
 pub mod dmcfe_ip;
